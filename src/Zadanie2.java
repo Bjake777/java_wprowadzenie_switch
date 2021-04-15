63205271cd720ba701d9b299e7c96c205554d750
Michal Bujak <michalbujak7777@gmail.com>
2021-04-15 10:37:41 +0200

    dodanie zadania 2

---
+++ src/Zadanie2.java
@@ -0,0 +1,19 @@
+public class Zadanie2 {
+    public static void main(String[] args) {
+        char firstLetter = 'c';
+
+        switch (firstLetter){
+            case 'c':
+                System.out.println("Czerwony");
+                break;
+            case 'z':
+                System.out.println("Trawa");
+                break;
+            case 'n':
+                System.out.println("Morze");
+                break;
+            default:
+                System.out.println("Brak skojarzeń");
+        }
+    }
+}
